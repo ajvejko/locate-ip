c7344f3d0a2217ff8115f6e81364985a6fc3a9ee
ajvejko <moucka.matej01@gmail.com>
2023-10-25 23:24:15 +0200

    added color options and fonts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,25 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  app: {
+    head: {
+      link: [
+        { rel: "preconnect", href: "https://fonts.googleapis.com" },
+        {
+          rel: "preconnect",
+          href: "https://fonts.gstatic.com",
+        },
+        {
+          rel: "stylesheet",
+          href: "https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@100;200;300;400;600&display=swap",
+        },
+        {
+          rel: "stylesheet",
+          href: "https://fonts.googleapis.com/css2?family=Poppins:wght@200;400;700&display=swap",
+        },
+      ],
+    },
+  },
   css: ["~/assets/css/main.css"],
   postcss: {
     plugins: {
@@ -8,4 +27,5 @@ export default defineNuxtConfig({
       autoprefixer: {},
     },
   },
+  ssr: false,
 });
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,8 +8,29 @@ export default {
     "./app.vue",
     "./error.vue",
   ],
+  darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        roboto: "'Roboto', sans-serif",
+        poppins: "'Poppins', sans-serif",
+      },
+      colors: {
+        // Light theme
+        textLight: "#1d2b35",
+        bgLight: "#eff0f1",
+        primaryLight: "#b64a02",
+        secondaryLight: "#c8e8fe",
+        accentLight: "#f76402",
+
+        // Dark theme
+        textDark: "#c9d8e2",
+        bgDark: "#0F1011",
+        primaryDark: "#fd9149",
+        secondaryDark: "#012238",
+        accentDark: "#fd6a08",
+      },
+    },
   },
   plugins: [],
 };
