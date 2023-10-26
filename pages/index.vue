ec20778e7c989c7aa328cb235289e2a9b2773da5
ajvejko <moucka.matej01@gmail.com>
2023-10-27 00:41:20 +0200

    added title to index

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,12 @@
 <template>
   <section
-    class="bg-bgLight text-textLight dark:text-textDark dark:bg-bgDark min-h-screen px-3"
+    class="bg-bgLight text-textLight dark:text-textDark dark:bg-bgDark min-h-screen px-3 pt-16"
   >
-    <div>Hello from pages!</div>
+    <div class="flex justify-center">
+      <span
+        class="font-poppins bg-gradient-to-r from-rose-500 via-red-500 to-orange-500 bg-clip-text text-3xl font-bold text-transparent"
+        >Locate the IP!</span
+      >
+    </div>
   </section>
 </template>
